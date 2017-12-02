7ac7282f34adf222b5ddb0ea43b0cb2a90291d27
Frans Simanjuntak <franzteve@gmail.com>
2017-12-02 02:01:33 +0100

    Delete old files

--- assignment2/GraphQL/GraphQLAPI/graphql_query.py
+++
@@ -1,9 +0,0 @@
-from schema import User, Listing, Review
-import json2obj
-
-class Query(ObjectType):
-    reviews = List(Review, id=Int(required=True))
-
-    def resolve_reviews(self, args, context, info):
-        reviews = api_call(args.get("id"))["reviews"]
-        return json2obj(json.dumps(reviews))
--- assignment2/GraphQL/GraphQLAPI/json2obj.py
+++
@@ -1,7 +0,0 @@
-# stolen from https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object/15882054#15882054
-
-def _json_object_hook(d):
-    return namedtuple('X', d.keys())(*d.values())
-
-def json2obj(data):
-    return json.loads(data, object_hook=_json_object_hook)
